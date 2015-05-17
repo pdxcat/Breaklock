fa6e3ed19dd3d9672b562983b96a0751f8b7a75a
Tyler Burdsall <tylerburdsall@gmail.com>
2015-05-17 15:12:18 -0700

    Cleaned up the code
    Placed the send spam into a separate function. Takes in the correct arguments.

--- breaklock.ps1
+++ breaklock.ps1
@@ -1,4 +1,4 @@
-﻿param(
+param(
     [Parameter(Mandatory=$true)][String]$ComputerName,
     [Switch]$Force
 )
@@ -29,8 +29,40 @@ if (-not $sessions) {
         try {
             $session.Logoff()
             Write-Host "`nLock broken for $($session.UserAccount) on $ComputerName.`n"
+            send_spam($($session.UserName), $ComputerName)
         } catch {
             Write-Error "`nBreaklock failed for $($session.UserAccount) on $ComputerName!`n"
         }
+    }
+}
+
+##thunderburd##
+function send_spam ($user, $ComputerName) 
+{
+    ##Date and time for the email message
+    $date = (Get-Date).ToShortDateString()
+    $time = (Get-Date).ToShortTimeString()
+    
+    ##Email settings
+    $Smtp = "mailhost.cecs.pdx.edu"
+    $From = "support@cat.pdx.edu"
+    $To = "$user@cecs.pdx.edu"
+    $Subject = "Your Windows Session has been Terminated"
+    $CC = "support@cat.pdx.edu"
+    
+    ##Body Paragraph of the Email, broken up for easier reading##
+    $Body = "At approximately $time, on $date, I found you to have locked the screen on $ComputerName."
+    $Body += " These machines are to be left open to other users as much as possible. If you need to step away"
+    $Body += " from the computer for more than 15 minutes, you are required to log out."
+    $Body += "`n`nIf you have a class project that requires you to be logged in for an extended period of time please run hiberfoo by completing the following steps:"
+    $Body += "`n`n1. Open My Computer`n2. In the address bar type: \\frost\programs\programs\hiberfoo`n3. Double Click on the Hiberfoo icon`n4. Click submit."
+    $Body += "`n`nIn the meantime, we have terminated your session on $ComputerName."
+
+    ##Try to send the email. If it fails, display an error message
+    try{
+        Send-MailMessage -SmtpServer $Smtp -From $From -To $To -Subject $Subject -Body $Body -Cc $CC
+        Write-Host "`nBreaklock spam sent to $To."
+    } catch {
+        Write-Error "`nSpam failed to send, please send manually."
     }
 }
